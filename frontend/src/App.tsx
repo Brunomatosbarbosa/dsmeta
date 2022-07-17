31b1a20f1547d1938185af7ad853e7d29a438f9b
bruno.mbarbosa <bruno.mbarbosa@b2wdigital.com>
2022-07-17 14:12:05 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import NotificationButton from "./components/NotificationButton"
 import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
+    <ToastContainer />
       <Header />
       <main>
         <section id="sales">
